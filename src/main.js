37c0ee3a2e5ae4e38501dd3e11898ad610337965
lufranski <lufranski@gmail.com>
2022-12-14 20:57:04 +0100

    FontAwesome Dep Installed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+
+import {library} from '@fortawesome/fontawesome-svg-core'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+library.add(faChevronDown);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
