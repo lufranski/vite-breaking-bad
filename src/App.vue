40aa52fcad6d0e465b19b73e7361538d4e7cd575
lufranski <lufranski@gmail.com>
2022-12-14 16:10:11 +0100

    AppDarkBar created and imported

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,57 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import AppDarkBar from './components/AppDarkBar.vue'
+  
+  export default {
+      components: {
+        AppDarkBar
+      }    
+  }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="container inner">
+        <div class="label">
+            Select category
+        </div>
+
+        <section id="characters">
+            <AppDarkBar />
+        </section>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style lang="scss">
+@use './style/general.scss' as *;
+@use './style/partials/variables' as *;
+
+.container {
+  background-color: $bg-color;
+  padding: .5rem;
+  // DEBUG
+  height: 300px;
+
+  .inner {
+    width: 80%;
+    // DEBUG
+    border: 1px solid #fff;
+
+    .label {
+      background-color: $light-color;
+      border-radius: 5px;
+      padding: .5rem;
+    }
+
+    #characters {
+      background-color: $light-color;
+      width: 90%;
+      padding: .2rem;
+      margin-top: 2px;
+      // DEBUG
+      min-height: 70%;
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/AppDarkBar.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+
+    }
+</script>
+
+<template>
+    <div>
+        <p>Found 62 characters</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+
+    div {
+        background-color: $dark-color;
+        padding: .2rem;
+        // DEBUG
+        height: 100px;
+
+        p {
+            color: $light-color;
+        }
+    }
+</style>
