bb6799166e38578a061a039a4dccf6ee9d429dd6
lufranski <lufranski@gmail.com>
2022-12-15 18:39:55 +0100

    API page change

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import {reactive} from 'vue'
 
 export const store = reactive ({
     characterList: [],
-    apiUrl: 'https://rickandmortyapi.com/api/character',
+    apiUrl: 'https://rickandmortyapi.com/api/character?page=13',
     filterText: '',
     statusApi: 'status',
 });
